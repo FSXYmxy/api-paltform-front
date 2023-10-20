7ee5c1cdda2f99b2483ef61d8d2d1373d43b116b
mengxiangyue <3133194716@qq.com>
2023-10-20 11:11:45 +0800

    完善基本功能，并更换服务器

--- config/proxy.ts
+++ config/proxy.ts
@@ -15,7 +15,7 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'http://101.34.243.75:8101',
+      target: 'http://117.72.39.26:8101',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
